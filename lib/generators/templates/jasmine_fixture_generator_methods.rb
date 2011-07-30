6367a73f5c3aa5ca629ce1c4d39a00fc5239237b
lporras16@gmail.com <lporras16@gmail.com>
2011-07-30 18:42:16 -0500

    adding jasmine-jquery and jasmine_fixture_generator_methods.rb, adding nokogiri on gemspec

---
+++ lib/generators/templates/jasmine_fixture_generator_methods.rb
@@ -0,0 +1,46 @@
+# Add fixture-generation methods to ControllerExampleGroup. We load
+# this file within our spec_helper.rb
+
+RSpec::Rails::ControllerExampleGroup.extend(FixtureHelperMethods)
+
+module FixtureHelperMethods
+  # Saves the markup to a fixture file using the given name
+  def save_fixture(markup, name)
+    fixture_path = File.join(Rails.root, '/spec/javascripts/fixtures')
+    Dir.mkdir(fixture_path) unless File.exists?(fixture_path)
+
+    fixture_file = File.join(fixture_path, "#{name}.html")
+    File.open(fixture_file, 'w') do |file|
+      file.puts(markup)
+    end
+  end
+
+  # From the controller spec response body, extracts html identified
+  # by the css selector.
+  def html_for(selector)
+    doc = Nokogiri::HTML(response.body)
+
+    remove_third_party_scripts(doc)
+    content = doc.css(selector).first.to_s
+
+    convert_body_tag_to_div(content)
+  end
+
+  # Recommended that you add all body-level third party scripts inside a div called #third-party-scripts
+  # so that they can be removed during testing.
+  def remove_third_party_scripts(doc)
+    scripts = doc.at('#third-party-scripts')
+    scripts.remove if scripts
+  end
+
+  # Many of our css and jQuery selectors rely on a class attribute we
+  # normally embed in the <body>. For example:
+  #
+  # <body class="workspaces show">
+  #
+  # Here we convert the body tag to a div so that we can load it into
+  # the document running js specs without embedding a <body> within a <body>.
+  def convert_body_tag_to_div(markup)
+    markup.gsub("<body", '<div').gsub("</body>", "</div>")
+  end
+end
