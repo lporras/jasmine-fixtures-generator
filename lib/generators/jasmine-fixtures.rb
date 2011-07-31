b5b34249c29feb4b9e9cdd614ff0d4487737eafb
lporras16@gmail.com <lporras16@gmail.com>
2011-07-30 19:21:16 -0500

    adding methods to generator to copy jasmine-jquery and jasmine_fixtures_generator_methods

--- lib/generators/jasmine-fixtures.rb
+++ lib/generators/jasmine-fixtures.rb
@@ -1,3 +1,15 @@
 class JasmineFixturesGenerator < Rails::Generators::Base
 
+  desc "This generator install jasmine-jquery and add jasmine_fixture_generator_methods.rb"
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_jasmine_jquery
+    say_status("copying", "jasmine-jquery", :green)
+    copy_file "../../../../vendor/jasmine-jquery.js", "spec/javascripts/helpers/jasmine-jquery.js"
+  end
+
+  def copy_jasmine_fixture_generator_methods
+    say_status("copying", "jasmine_fixture_generator_methods.rb", :green)
+    copy_file "jasmine_fixture_generator_methods.rb", "spec/support/jasmine_fixture_generator_methods.rb"
+  end
 end
